48cbfca7656a0bb2bcf1871c4e97ded52415cd23
Beat <ggabrieljimenezmartinez@gmail.com>
2022-09-05 11:19:01 -0500

    add envs

--- src/api/catalogs.js
+++ src/api/catalogs.js
@@ -1,7 +1,7 @@
 import axios from "axios"
+import { API_URL } from "./constans"
 
-export const catalogsPostalCode = (postalCode) =>
-  axios.get(`http://localhost:3000/api/v1/catalogs/postal_code?postal_code=${postalCode}`)
+export const catalogsPostalCode = (postalCode) => axios.get(`${API_URL}/catalogs/postal_code?postal_code=${postalCode}`)
 
 export const catalogsSuburbsByPostalCode = (postalCode) =>
-  axios.get(`http://localhost:3000/api/v1/catalogs/suburbs?postal_code=${postalCode}`)
+  axios.get(`${API_URL}/catalogs/suburbs?postal_code=${postalCode}`)
