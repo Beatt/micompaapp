c41009495d9567f5b716191e4f0a41121f01f359
Gabriel Jiménez <ggabrieljimenezmartinez@gmail.com>
2022-09-05 15:35:32 -0500

    [Feature] add envs (#10)

--- src/api/addresses.js
+++ src/api/addresses.js
@@ -1,19 +1,12 @@
 import axios from "axios"
+import { API_URL, HEADER_JSON } from "./constans"
 
-const HEADERS = {
-  headers: {
-    "Content-Type": "application/json",
-  },
-}
+export const addressesCreate = (data) => axios.post(`${API_URL}/addresses`, JSON.stringify(data), HEADER_JSON)
 
-export const addressesCreate = (data) =>
-  axios.post("http://localhost:3000/api/v1/addresses", JSON.stringify(data), HEADERS)
+export const addressesUpdate = (id, data) => axios.put(`${API_URL}/addresses/${id}`, JSON.stringify(data), HEADER_JSON)
 
-export const addressesUpdate = (id, data) =>
-  axios.put(`http://localhost:3000/api/v1/addresses/${id}`, JSON.stringify(data), HEADERS)
+export const addressesGetOne = (id) => axios.get(`${API_URL}/addresses/${id}`)
 
-export const addressesGetOne = (id) => axios.get(`http://localhost:3000/api/v1/addresses/${id}`)
+export const addressesGetMany = () => axios.get(`${API_URL}/addresses`)
 
-export const addressesGetMany = () => axios.get("http://localhost:3000/api/v1/addresses")
-
-export const addressesDelete = (id) => axios.delete(`http://localhost:3000/api/v1/addresses/${id}`)
+export const addressesDelete = (id) => axios.delete(`${API_URL}/addresses/${id}`)
